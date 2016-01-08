83a4abf5d6220729c44afebbec9e197d0e6568ff
NengQuan <1020725009@qq.com>
2016-01-08 15:25:01 +0800

    Initial Commit

---
+++ CityListViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CityListViewController
+//
+//  Created by NengQuan on 16/1/8.
+//  Copyright © 2016年 NengQuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CityListViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CityListViewController
+//
+//  Created by NengQuan on 16/1/8.
+//  Copyright © 2016年 NengQuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
